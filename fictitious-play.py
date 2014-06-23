15bcd59492cae71b3bb7542947f2d2819bd95379
knkszk0602 <k.suzuki0602@gmail.com>
2014-06-23 22:48:32 +0900

    遅くなってすみません。。。

---
+++ fictitious-play.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid.axislines import SubplotZero
+from random import uniform
+import numpy as np
+
+ts_length = 2000
+pay0 = np.array([[1, -1],[-1, 1]])
+pay1 = np.array([[-1, 1],[1, -1]])
+cur_x0, cur_x1 = uniform(0, 1), uniform(0, 1)
+x0s = [cur_x0]
+x1s = [cur_x1]
+
+for i in range(ts_length):
+    
+    pro0 = np.array([1-cur_x1, cur_x1])
+    pro1 = np.array([1-cur_x0, cur_x0])
+    exp0 = np.dot(pay0, pro1)
+    exp1 = np.dot(pay1, pro0)
+
+    if exp0[0] > exp0[1]:
+        cur_a0 = 0
+    elif exp0[0] < exp0[1]:
+        cur_a0 = 1
+    else:
+        cur_a0 = random.choice([0, 1])
+        
+    if exp1[0] > exp1[1]:
+        cur_a1 = 0
+    elif exp1[0] < exp1[1]:
+        cur_a1 = 1
+    else:
+        cur_a1 = random.choice([0, 1])
+
+    x0s.append(cur_x0)
+    x1s.append(cur_x1)
+    cur_x0 = cur_x0 + (cur_a1 - cur_x0)/(i + 2)
+    cur_x1 = cur_x1 + (cur_a0 - cur_x1)/(i + 2)
+fig, ax = plt.subplots()
+ax.plot(x0s, 'r-')
+ax.plot(x1s, 'b-')
+#plt.savefig('fictitious.png')
+#plt.savefig('fictitious.pdf')
+plt.show()
+
+fig = plt.figure()
+ax = SubplotZero(fig, 111)
+fig.add_subplot(ax)
+ax.hist(x0s, bins = 50)
+#plt.savefig('fictitious_hist.png')
+#plt.savefig('fictitious_hist.pdf')
+plt.show()
+
