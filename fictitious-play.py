1eeb024cebc205be9cfcc001a1236fba3efc0127
knkszk0602 <k.suzuki0602@gmail.com>
2014-06-27 16:23:22 +0900

    modified

--- fictitious-play.py
+++ fictitious-play.py
@@ -4,38 +4,51 @@ from mpl_toolkits.axes_grid.axislines import SubplotZero
 from random import uniform
 import numpy as np
 
-ts_length = 2000
-pay0 = np.array([[1, -1],[-1, 1]])
-pay1 = np.array([[-1, 1],[1, -1]])
-cur_x0, cur_x1 = uniform(0, 1), uniform(0, 1)
-x0s = [cur_x0]
-x1s = [cur_x1]
+t = 1000
 
-for i in range(ts_length):
+def fict(t):
+    pay0 = np.array([[1, -1], [-1, 1]])
+    pay1 = np.array([[-1, 1], [1, -1]])
+    cur_x0, cur_x1 = uniform(0, 1), uniform(0, 1)
+    x0s = []
+    x1s = []
+
+    for i in range(t):
+        pro0 = np.array([1-cur_x1, cur_x1])
+        pro1 = np.array([1-cur_x0, cur_x0])
+        exp0 = np.dot(pay0, pro1)
+        exp1 = np.dot(pay1, pro0)
     
-    pro0 = np.array([1-cur_x1, cur_x1])
-    pro1 = np.array([1-cur_x0, cur_x0])
-    exp0 = np.dot(pay0, pro1)
-    exp1 = np.dot(pay1, pro0)
+        if exp0[0] > exp0[1]:
+            cur_a0 = 0
+        elif exp0[0] < exp0[1]:
+            cur_a0 = 1
+        else:
+            cur_a0 = random.choice([0, 1])
+            
+        if exp1[0] > exp1[1]:
+            cur_a1 = 0
+        elif exp1[0] < exp1[1]:
+            cur_a1 = 1
+        else:
+            cur_a1 = random.choice([0, 1])
 
-    if exp0[0] > exp0[1]:
-        cur_a0 = 0
-    elif exp0[0] < exp0[1]:
-        cur_a0 = 1
-    else:
-        cur_a0 = random.choice([0, 1])
-        
-    if exp1[0] > exp1[1]:
-        cur_a1 = 0
-    elif exp1[0] < exp1[1]:
-        cur_a1 = 1
-    else:
-        cur_a1 = random.choice([0, 1])
+        x0s.append(cur_x0)
+        x1s.append(cur_x1)
+        cur_x0 = cur_x0 + (cur_a1 - cur_x0)/(i + 2)
+        cur_x1 = cur_x1 + (cur_a0 - cur_x1)/(i + 2)
 
-    x0s.append(cur_x0)
-    x1s.append(cur_x1)
-    cur_x0 = cur_x0 + (cur_a1 - cur_x0)/(i + 2)
-    cur_x1 = cur_x1 + (cur_a0 - cur_x1)/(i + 2)
+    return x0s, x1s
+
+def ficthist(t):
+    x0_last = []
+    for i in range(t):
+        x0s, x1s = fict(t)
+        x0_last.append(x0s[-1])
+    return x0_last
+
+x0s, x1s = fict(t)
+
 fig, ax = plt.subplots()
 ax.plot(x0s, 'r-')
 ax.plot(x1s, 'b-')
@@ -43,11 +56,12 @@ ax.plot(x1s, 'b-')
 #plt.savefig('fictitious.pdf')
 plt.show()
 
+
 fig = plt.figure()
 ax = SubplotZero(fig, 111)
 fig.add_subplot(ax)
-ax.hist(x0s, bins = 50)
-#plt.savefig('fictitious_hist.png')
-#plt.savefig('fictitious_hist.pdf')
+ax.hist(ficthist(t))
+plt.savefig('fictitious_hist.png')
+plt.savefig('fictitious_hist.pdf')
 plt.show()
 
